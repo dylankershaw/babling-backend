a12e9f5f308bfc4d5c733d4e1afc2e0802e3e2c8
github username <dylankershaw@me.com>
2017-12-01 17:14:46 -0500

    serializer test

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
-  has_many :chats, serializer: UserChatsSerializer
+  # has_many :chats, serializer: UserChatsSerializer
+  has_many :chats, embed: :ids, include: false
 end

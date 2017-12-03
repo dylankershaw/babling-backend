6b01a3e3fd61e31b1cdb5514cec4115e4dbc3462
github username <dylankershaw@me.com>
2017-12-02 19:10:03 -0500

    updated user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:name)
+        params.permit(:username, :password)
     end
 
 end

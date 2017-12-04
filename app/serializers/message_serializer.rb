bbe52e0a35f799bd996294b0169ec822a9156f91
github username <dylankershaw@me.com>
2017-12-04 11:12:21 -0500

    updated message serializer with sender_name

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,4 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :chat_id, :sender_id, :created_at, :updated_at
+  attributes :id, :chat_id, :sender_name, :created_at, :updated_at
   has_many :translations
 end

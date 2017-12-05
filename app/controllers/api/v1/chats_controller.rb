e6193b6ab18845b888d047ecb34c47626740f9c5
github username <dylankershaw@me.com>
2017-12-05 16:22:04 -0500

    working translations on language change

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ChatsController < ApplicationController
 
+    @@translator = Yandex::Translator.new('trnsl.1.1.20171204T203148Z.edb25d6878d750e4.eb231d93b0ca2ae6937947ed9a0188df3a07ca10')
+
     def index
         @chats = Chat.all
         render json: @chats
@@ -20,9 +22,32 @@ class Api::V1::ChatsController < ApplicationController
 
     def update
         @chat = Chat.find(params[:id])
-
-        @chat.languages << params["languages"] if !@chat.languages.include?(params["languages"])
         
+        # iterate through each of the chat's messages and translate to the new language
+        @chat.messages.each do |message|
+            # check to see if the message already has a translation in that language
+            if !@chat.languages.include?(params["languages"])
+                # translate the message to french
+                translated_text = @@translator.translate message.translations.first.text, from: message.translations.first.language, to: chat_params["languages"]
+                language = chat_params["languages"]
+
+                # create the translation
+                new_translation = Translation.create(
+                    language: language,
+                    message_id: message.id,
+                    text: translated_text
+                )
+                
+                # associate the translation with the message
+                message.translations << new_translation
+                message.save # is this necessary?
+
+                @chat.languages << language
+                @chat.save # is this necessary?
+            end
+
+        end
+
         # @chat.update(chat_params)
         if @chat.save
             render json: @chat

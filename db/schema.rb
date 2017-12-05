b9c42bb7fbfcf808235895291330de2c649f0eab
github username <dylankershaw@me.com>
2017-12-04 19:28:54 -0500

    changed chats default value to include en

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204181105) do
+ActiveRecord::Schema.define(version: 20171205002810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chats", force: :cascade do |t|
-    t.string "languages", default: [], array: true
+    t.string "languages", default: ["en"], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

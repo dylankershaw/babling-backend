1dfc2eb55a8659b0297eaa967c4a2a3f0ff48818
github username <dylankershaw@me.com>
2017-12-05 11:29:56 -0500

    working translation creation

--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -1,26 +1,30 @@
 class Api::V1::TranslationsController < ApplicationController
 
-    translator = Yandex::Translator.new('trnsl.1.1.20171204T203148Z.edb25d6878d750e4.eb231d93b0ca2ae6937947ed9a0188df3a07ca10')
+    @@translator = Yandex::Translator.new('trnsl.1.1.20171204T203148Z.edb25d6878d750e4.eb231d93b0ca2ae6937947ed9a0188df3a07ca10')
 
     def show
         @translation = Translation.find(params[:id])
         render json: @translation
     end
-    
-    def translate(text, fromLang, toLang)
-        translator.translate text, from: fromLang, to: toLang
-    end
 
-    def create
+    def create()
         @translation = Translation.create(translation_params)
         # associate the translation with the message
         Message.last.translations << @translation
         # translate the message to every other language in the chat.languages array
         (@translation.message.chat.languages - [@translation.language]).each do |language|
-            # fetch the yandex api and instantiate the result as a new translation
-            translation = translator.translate @translation.text, from: @translation.language, to: language
-            binding.pry
-            # relate that new translation to the message
+            text = @translation.text
+            fromLang = @translation.language
+            toLang = language
+            translation = @@translator.translate text, from: fromLang, to: toLang
+            # instantiate the translation
+            new_translation = Translation.create(
+                message_id: @translation.message.id,
+                language: toLang,
+                text: translation
+            )
+            # associate the new translation with the message
+            @translation.message.translations << new_translation
         end
     end
 
